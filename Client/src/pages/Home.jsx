67eaed54abef38eff08e9ddcd577df97e12fbc3b
Ujjwal Anand <uacode75@gmail.com>
2025-08-05 21:37:30 +0530

    Created slider

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
 import Steps from '../components/Steps.jsx'
+import BgSlider from '../components/BgSlider.jsx'
 
 function Home() {
   return (
     <div>
       <Header />
       <Steps />
+      <BgSlider />
     </div>
   )
 }
